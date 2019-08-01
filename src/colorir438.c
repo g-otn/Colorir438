6da16e7d6d40ffda212ba23bbc4397d1a97070d1
g-otn <g-otn@hotmail.com>
2019-08-01 05:34:36 -0300

    Adicionar verificações de arquivo em lerColorir438

--- src/colorir438.c
+++ src/colorir438.c
@@ -14,20 +14,42 @@ Imagem lerColorir438(FILE *arquivo)
     Imagem img = criarImagem(alt, lar);
 
     // Lê os valores dos pixels
-    fseek(arquivo, 2, SEEK_CUR);
-    for (int i = 0; i < alt; i++)
-        for (int j = 0; j < lar; j++)
+    fseek(arquivo, 2, SEEK_CUR); // Vai para o começo da 'matriz' de pixels
+    for (int y = 0; y < alt; y++)
+        for (int x = 0; x < lar; x++)
         {
             int ch = fgetc(arquivo);
-            if (ch == '\n')
-                ch = fgetc(arquivo);
-            if (ch < 58)
-                img.pixels[i][j] = ch - 48; // Converte do int (48-57) = char ('0'-'9') para int (48-57)-48 = cores (0-9)
-            else
-                img.pixels[i][j] = ch - 55; // Converte do int (65-70)+55 = char ('A'-'F') para int (65-70) - 55 = cores (10-16)
-        }
-    fclose(arquivo);
 
+            if (ch == EOF) { // Encerra leitura se acabou os caracteres
+                fclose(arquivo);
+                c(14); printf("A arquivo (%dx%d) possui %d pixels nao-preenchidos (%d/%d), a partir de (x%d, y%d). Eles aparecerao em preto.", img.lar, img.alt, (img.lar * img.alt) - (y * img.lar + x), y * img.lar + x, img.lar * img.alt, x, y);
+                _getch();
+                return img;
+            }
+
+            // Ignora quebra de linha e espaço
+            while (ch == '\n' || ch == ' ')
+                ch = fgetc(arquivo);
+
+            if (ch == EOF) { // Encerra leitura se acabou os caracteres
+                fclose(arquivo);
+                c(14); printf("A arquivo (%dx%d) possui %d pixels nao-preenchidos (%d/%d), a partir de (x%d, y%d). Eles aparecerao em preto.", img.lar, img.alt, (img.lar * img.alt) - (y * img.lar + x), y * img.lar + x, img.lar * img.alt, x, y);
+                _getch();
+                return img;
+            }
+            
+            if (ch < 58)
+                img.pixels[y][x] = ch - 48; // Converte do int (48-57) = char ('0'-'9') para int (48-57)-48 = cores (0-9)
+            else
+                img.pixels[y][x] = ch - 55; // Converte do int (65-70)+55 = char ('A'-'F') para int (65-70) - 55 = cores (10-16)
+        }
+    
+
+    if (fgetc(arquivo) != EOF) {
+        c(14); printf("O arquivo possui mais pixels do que sua resolucao suporta (%dx%d). Eles serao ignorados.", img.lar, img.alt);
+    }
+
+    fclose(arquivo);
     return img;
 }
 
